5498f332065dcf362fe7aa44bda11acb8de05b12
Vu-Tien-Duc <vutienduc05@gmail.com>
2025-08-22 14:55:17 +0700

    update 2:55

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,27 +12,53 @@ public class HomeController : Controller
     }
 
     // GET: /Home/Index
-    public async Task<IActionResult> Index(string? searchString, int? categoryId)
+    public async Task<IActionResult> Index(string? searchString, int? categoryId, string? sortOrder, int page = 1, int pageSize = 6)
     {
-        // Lấy tất cả thể loại đưa sang ViewBag
-        ViewBag.Categories = await _context.LoaiSaches.ToListAsync();
-        ViewBag.SelectedCategory = categoryId;
+        var sach = _context.Saches.AsQueryable();
 
-        var sach = from s in _context.Saches
-                   select s;
-
+        // Lọc theo từ khóa
         if (!string.IsNullOrEmpty(searchString))
         {
             sach = sach.Where(s => s.TenSach.Contains(searchString));
         }
 
+        // Lọc theo thể loại
         if (categoryId.HasValue)
         {
             sach = sach.Where(s => s.MaLoai == categoryId);
         }
 
-        return View(await sach.ToListAsync());
+        // Sắp xếp
+        ViewBag.CurrentSort = sortOrder;
+        ViewBag.PriceSort = sortOrder == "price_asc" ? "price_desc" : "price_asc";
+
+        switch (sortOrder)
+        {
+            case "price_asc":
+                sach = sach.OrderBy(s => s.GiaBan);
+                break;
+            case "price_desc":
+                sach = sach.OrderByDescending(s => s.GiaBan);
+                break;
+            default:
+                sach = sach.OrderBy(s => s.TenSach);
+                break;
+        }
+
+        // Phân trang
+        int totalItems = await sach.CountAsync();
+        var items = await sach.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+
+        ViewBag.TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+        ViewBag.CurrentPage = page;
+
+        // Thể loại
+        ViewBag.Categories = await _context.LoaiSaches.ToListAsync();
+        ViewBag.SelectedCategory = categoryId;
+
+        return View(items);
     }
+
 
     // GET: /Home/Details/5
     public async Task<IActionResult> Details(int? id)
--- obj/Debug/net8.0/QLBanSachWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/QLBanSachWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLBanSachWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f763e7aceb8715d659ca4ba66e451b960078226f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a88095ce161895106698eb0f09afbe5b9de4cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLBanSachWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLBanSachWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
